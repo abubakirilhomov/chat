be7148232b0ec810f1a206646589684f3d8dbc01
abubakirilhomov <2ilhomovabubakir2@gmail.com>
2024-06-24 17:31:14 +0500

    rep

--- client/src/components/ChatAndQuiz.js
+++ client/src/components/ChatAndQuiz.js
@@ -8,14 +8,12 @@ const ChatAndQuiz = () => {
   const { roomId } = useParams(); // Получаем номер комнаты из URL
 
   return (  
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <div className='absoulte left-3 top-7'>
-        <ExitButton />
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className=''>
+        <ExitButton room={roomId}/>
       </div>
-      <div className="w-full max-w-md p-4 bg-white shadow-md rounded-md flex flex-col gap-4">
         <Chat room={roomId} /> 
         <Quiz room={roomId}/>
-      </div>
     </div>
   );
 };
